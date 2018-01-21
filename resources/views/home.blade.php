cff33d5b98911395f0940d23d4cbd0fd64601ee2
Cory Monteleone <cmonteleoneh@gmail.com>
2018-01-20 19:43:13 -0500

    Adds written copy and flexbox fix to vertically align About Us graphic with copy.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
 	<p>We are located in the heart of Historic Pitman New Jersey just off of the ever-popular Broadway strip. With lots of small, quirky shops around the strip, Endgrain is perfectly located to caffeinate your trip to Uptown Pitman.</p>
 @stop
 @section('aboutus')
-	<p>Endgrain coffee is a small coffee bar in Pitman, NJ specializing in handcrafted, high quality coffee drinks. We were the first to have a Nitro iced coffee tap in south Jersey and are constantly working to create new and fun coffee based drinks for our customers. We strive to provide the best coffee experience in the area specializing in small batch, high quality blends and Single Origin beans.</p>
-  <p></p>
+	<p>Endgrain coffee is a small coffee bar in Pitman, NJ specializing in handcrafted, high quality coffee drinks. We were the first to have a Nitro iced coffee tap in south Jersey and are constantly working to create new and fun coffee based drinks for our customers. We strive to provide the best coffee experience in the area specializing in small batch, high quality blends and Single Origin beans. Endgrain frequently works with local businesses to provide a range of partnerships and services such as wholesale beans, coffee concentrates for Pitman's Kelly Green Brewing Company coffee porter beer, and espresso concentrates for Pitman's Alaura Kitchen and Candy espresso ice cream.</p>
+  <p>In 2016 we began roasting our own coffee beans, allowing us to provide a wide variety of craft roasted coffees from around the world. Whether you're looking for fruity Ethiopian beans, chocolatey Central American beans, or premium geisha bean, there's something for you. Just ask one of our friendly baristas for help picking out the perfect roast to caffeinates your day. Try your beans in a pourover and take a bag home to keep the goodness going. If you can't stop by, that's not a problem. Endgrain operates on online store where you can buy cold brew pouches, whole beans, merch, gift certificates, travel mugs, and more.</p>
 @stop
 @section('services')
 	<div class="table-responsive col-md-3"> 
