8366ffa7c36ba9ff4a333395ae9dfdbf95f0ebd5
Cory Monteleone-Haught <cmonteleoneh@gmail.com>
2018-03-14 21:19:12 -0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
 @extends('templates.default')
+@section('alerts')
+	<h1>Check out our store for new beans.</h1>
+	<h3>Everything from decaf to special edition blends. <strong><a href="http://store.endgrain.coffee"> Shop now...</a></strong></h3>
+@stop
 @section('intro')
 	<p>Endgrain Coffee Bar is proud to introduce the first Nitro tap in South Jersey. Nitro is a slightly effervescent, ice-cold coffee on tap that is equal parts chocolatey and silky smooth; Cold brewed over 24 hours, and then placed under nitrogen pressure to add a smooth creaminess to coffee. Nitro will excite your taste buds and is a summer time cold coffee you can get excited about. Come in and give it a try today.</p>
 	<p>We are located in the heart of Historic Pitman New Jersey just off of the ever-popular Broadway strip. With lots of small, quirky shops around the strip, Endgrain is perfectly located to caffeinate your trip to Uptown Pitman.</p>
