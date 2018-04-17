6cb2a63585ccbf4a1de5664bfabed0e9e3c39ef8
Cory Monteleone-Haught <cmonteleoneh@gmail.com>
2018-04-17 15:18:48 -0400

    Update alerts.blade.php

--- resources/views/templates/snippets/alerts.blade.php
+++ resources/views/templates/snippets/alerts.blade.php
@@ -1,8 +1,9 @@
 <div data-example-id="dismissible-alert-js">
-	<div class="alert alert-info alert-dismissible fade in" role="alert" style="padding: 0px 15px; font-size: 16px !important;">
+	<div class="alert alert-success alert-dismissible fade in" role="alert" style="padding: 0px 15px; font-size: 16px !important;">
 		<button type="button" class="close" data-dismiss="alert" aria-label="Close" style="right: 0px; top: 0px;">
 			<span aria-hidden="true">×</span>
 		</button>
 		@yield('alerts')
+		<br>
 	</div>
 </div>
