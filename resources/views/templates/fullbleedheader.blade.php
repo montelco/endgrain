c85974313b0ddfa271f83fc9fb3cdae94ca6b7fb
Cory Monteleone <corymont@Corys-MacBook.hsd1.nj.comcast.net>
2018-08-03 19:41:25 -0400

    Update for iOS

--- resources/views/templates/fullbleedheader.blade.php
+++ resources/views/templates/fullbleedheader.blade.php
@@ -4,6 +4,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<title>Endgrain Coffee Bar | @yield('pageTitle')</title>
+		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 	    <meta name="mobile-web-app-capable" content="yes">
 	    <meta name="theme-color" content="#231F20">
