86b81eabdc9409eb2c72daa6afa216000427b21f
Cory Monteleone <corymont@Corys-MacBook.hsd1.nj.comcast.net>
2018-08-03 16:38:43 -0400

    Visual update for backgrounds

--- resources/views/templates/pagelets/singleColumn.blade.php
+++ resources/views/templates/pagelets/singleColumn.blade.php
@@ -1,10 +1,10 @@
 <section id="intro">
 	<div class="container">
 		<section class="row-fluid">
-			<div class="col-md-3">
+			<div class="col-md-3 col-sm-3">
 				@yield('featureImage','<img src="img/grinder.svg">')
 			</div>
-			<div class="col-md-9 col-sm-12">
+			<div class="col-md-9 col-sm-9">
 				<div class="page-header">
 					<h1>@yield('pageTitle')</h1>
 					<em>@yield('pageSubtitle')</em>	
