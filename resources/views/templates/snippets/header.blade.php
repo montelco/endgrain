500d389106450ce5978268ccf71a2ce76a0aa811
Cory Monteleone <corymont@Corys-MacBook.hsd1.nj.comcast.net>
2018-08-03 19:38:00 -0400

    Developer introduced bug is resolved

--- resources/views/templates/snippets/header.blade.php
+++ resources/views/templates/snippets/header.blade.php
@@ -1,6 +1,6 @@
 <header class="parabox">
 	<div class="logo">
 		<img src="https://endgrain.coffee/img/endgrain_full.svg" alt="Endgrain Coffee Bar Logo">
-    	<div class="store"><a hrer="http://store.endgrain.coffee">Shop Now</a></div>
+    	<div class="store"><a href="http://store.endgrain.coffee">Shop Now</a></div>
 	</div>
 </header>
