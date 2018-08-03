fdb220572080884bcda34f8f1a15f10424397487
Cory Monteleone <corymont@Corys-MacBook.hsd1.nj.comcast.net>
2018-08-03 18:28:09 -0400

    Added catering popular options

--- resources/views/templates/pagelets/singleColumn.blade.php
+++ resources/views/templates/pagelets/singleColumn.blade.php
@@ -15,14 +15,7 @@
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12 col-sm-12 copyBanner">
-				@yield('copyBanner')
-			</div>
-		</div>
-	</div>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12 col-sm-12 copyBanner">
+			<div class="col-md-12 col-sm-12">
 				@yield('body2')
 			</div>
 		</div>
