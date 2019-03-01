b19b500a3f4c17247bb49da59524740b52a18697
Cory Monteleone-Haught <cmonteleoneh@gmail.com>
2019-03-01 08:07:48 -0500

    Added file for presser on the new location

---
+++ resources/views/ten-pitman.blade.php
@@ -0,0 +1,25 @@
+@extends('templates.fullbleedheader')
+@section('featureImage')
+  <img class="feature" src="https://ucarecdn.com/d7908ead-cdab-42a8-a3e7-389e528c034c/-/progressive/yes/-/quality/lightest/-/scale_crop/600x600/endgrain-new-store-joe.jpg" width="480" alt="Joe Fultano, owner of Endgrain, poses in front of new storefront">
+  <br/>
+  <img class="feature" src="https://ucarecdn.com/d4b012a6-2370-4b45-b39d-17f677951dc1/-/progressive/yes/-/quality/lightest/-/scale_crop/600x600/tap.jpg" alt="Joe in 2015 standing in front of the original 2nd Ave location">
+@stop
+@section('pageTitle')
+  Things are a-brewin'
+@stop
+@section('pageSubtitle')
+  New location, new possibilities for Endgrain Coffee Bar
+@stop
+@section('body')
+  <p style="font-size: 16pt">After months of sleepless nights considering the option and taking the time to hash out details, Jeo Fultano, owner of Endgrain Coffee bar was able to say that the hard work had paid off. <a href="https://www.facebook.com/EndgrainCoffee/posts/2299869843599906" about="_blank">Taking to the Endgrain Coffee Bar Facebook page</a>, Joe announced that the deal was official as of February 28.</p>
+  <p style="font-size: 16pt">Pictured here, Joe had signed the papers on the original Second Ave location. Four years ago to the day, the Pitman community were first introduced to Endgrain Coffee Bar. Now, four years on, Endgrain is looking to grow by providing to community with a larger cafe space.</p>
+</div>
+@stop
+@section('body2')
+  <img src="/img/joe-new-store-presser-O.jpg" class="img-responsive" alt="Joe Fultano, owner of Endgrain, poses in front of new storefront"/>
+  <p style="font-size: 16pt">Joe stated on the Endgrain Facebook that, "This new space will not only allow us to have a much larger cafe but we will also move the roasting operation into the new space as well." With more cafe space and more space to allow for roasting, Endgrain will be taking fresh roasted coffee to the next level by having roasting just feet away from space where Endgrain's incredible team of baristas will be preparing each customer's coffee drinks.</p>
+  <p style="font-size: 16pt">Of course, Joe hasn't been at it alone. He took the time to thank his family, his awesome staff, and Endgrain's hugely loyal customers for believing and for coming back time and again.</p>
+  <p style="font-size: 24pt"><strong style="color: #555555; text-align: center;">I knew I kept that original poster around for a reason.</strong></p>
+  <p style="font-size: 16pt">As with any undertaking, Joe acknowledged that there's lots of work ahead but reassured his customers and the community that updates will be frequent.</p>
+  <p style="font-size: 16pt">Have questions? Feel free to <a href="#contact">get in touch</a> or send over an email to discuss any additional questions you may have: <strong><a href="mailto:endgraincoffee@gmail.com">Email Endgrain</a></strong></p>
+@stop
