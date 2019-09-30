4c5bc01cc2a0c2850e3048567b21dd6727bdaba2
Cory Monteleone-Haught <cmonteleoneh@gmail.com>
2019-09-30 10:45:30 -0400

    Update catering.blade.php

--- resources/views/catering.blade.php
+++ resources/views/catering.blade.php
@@ -1,8 +1,8 @@
 @extends('templates.fullbleedheader')
 @section('featureImage')
-	<img class="feature" src="https://scontent-iad3-1.cdninstagram.com/vp/658dd7bac6875ae133691aaaebe17e21/5C12E624/t51.2885-15/sh0.08/e35/s640x640/35617212_642802826074707_4451145385057976320_n.jpg" width="480" alt="Barista Joe Catering a Wedding">
+	<img class="feature" src="https://ucarecdn.com/ae098182-a80a-4327-afe6-c8f5a29be62a/35617212_642802826074707_4451145385057976320_n.jpg" width="480" alt="Barista Joe Catering a Wedding">
   <br/>
-  <img class="feature" src="https://scontent-iad3-1.cdninstagram.com/vp/d6de8ee82377dc8efc72a09853497e12/5C033FF7/t51.2885-15/e35/s480x480/21984969_1445900732191074_6112592301706444800_n.jpg" alt="Catering Camper">
+  <img class="feature" src="https://ucarecdn.com/34750832-611a-4bcd-bd5a-87a746bce6f3/21910166_1242570992555042_2567615716807147520_n.jpg" alt="Catering Camper">
 @stop
 @section('pageTitle')
 	Catering By Endgrain
@@ -23,16 +23,16 @@
       <tr>
         <td>
           <h3>Espresso Drinks</h3>
-          <p style="font-size: 14pt"><img class="img-responsive catering" src="https://scontent-iad3-1.cdninstagram.com/vp/66bba1bcbd3bf5b639f0a438db69cb6d/5C11C794/t51.2885-15/e35/s480x480/23596095_277084222812450_4897506634458202112_n.jpg" alt="Latte and Espresso">Lattes, Cappucinos, Americanos, Affogatos, oh my! One of our more popular options for on-site service, full service espresso drinks are a sure fire way to spice up any event. Whether you need to wake up that corporate event, get your students energized and excited, or kick off the wedding reception with an extra shot o' joe, our professional baristas will be ready to make any number of espresso drinks for your guests.</p>
+          <p style="font-size: 14pt"><img class="https://ucarecdn.com/bdf7d7f6-e308-4fd6-8d68-90b306c64521/37105647_201489290519178_2037428800416907264_n.jpg" alt="Latte and Espresso">Lattes, Cappucinos, Americanos, Affogatos, oh my! One of our more popular options for on-site service, full service espresso drinks are a sure fire way to spice up any event. Whether you need to wake up that corporate event, get your students energized and excited, or kick off the wedding reception with an extra shot o' joe, our professional baristas will be ready to make any number of espresso drinks for your guests.</p>
         </td>
-        <td class="hidden-xs"><img class="img-responsive catering" src="https://scontent-iad3-1.cdninstagram.com/vp/66bba1bcbd3bf5b639f0a438db69cb6d/5C11C794/t51.2885-15/e35/s480x480/23596095_277084222812450_4897506634458202112_n.jpg" alt="Latte and Espresso"></td>
+        <td class="hidden-xs"><img class="img-responsive catering" src="https://ucarecdn.com/bdf7d7f6-e308-4fd6-8d68-90b306c64521/37105647_201489290519178_2037428800416907264_n.jpg" alt="Latte and Espresso"></td>
       </tr>
       <tr>
         <td>
           <h3>Cold Brewed Coffee</h3>
-          <p style="font-size: 14pt"><img class="img-responsive catering" src="https://scontent-iad3-1.cdninstagram.com/vp/e9722de227abace8bf6c38edee42bc13/5C10A6B7/t51.2885-15/sh0.08/e35/s640x640/36561246_1770406549717427_6073674482050924544_n.jpg" alt="Cold Brew">The sweeter brother of iced coffee, cold brewed coffee never touches hot water which means that it's not bitter. The cold brewing process imbues rich, chocolatey notes and makes for a refreshing treat for your guests. Even if your guests take some coffee with their cup of cream, they'll be able to drink our sweet cold brew black.</p>
+          <p style="font-size: 14pt"><img class="img-responsive catering" src="https://ucarecdn.com/7c13f04b-d4fe-4872-bd4c-10b21b5b1ab4/18013950_398680717183217_7954317528551915520_n.jpg" alt="Cold Brew">The sweeter brother of iced coffee, cold brewed coffee never touches hot water which means that it's not bitter. The cold brewing process imbues rich, chocolatey notes and makes for a refreshing treat for your guests. Even if your guests take some coffee with their cup of cream, they'll be able to drink our sweet cold brew black.</p>
         </td>
-        <td class="hidden-xs"><img class="img-responsive catering" src="https://scontent-iad3-1.cdninstagram.com/vp/e9722de227abace8bf6c38edee42bc13/5C10A6B7/t51.2885-15/sh0.08/e35/s640x640/36561246_1770406549717427_6073674482050924544_n.jpg" alt="Cold Brew"></td>
+        <td class="hidden-xs"><img class="img-responsive catering" src="https://ucarecdn.com/7c13f04b-d4fe-4872-bd4c-10b21b5b1ab4/18013950_398680717183217_7954317528551915520_n.jpg" alt="Cold Brew"></td>
       </tr>
       <tr>
         <td>
@@ -44,9 +44,9 @@
       <tr>
         <td>
           <h3>Batch Brewed Coffee</h3>
-          <p style="font-size: 14pt"><img class="img-responsive catering" src="https://scontent-iad3-1.cdninstagram.com/vp/94c15cc6c09ac29881a67bddf4d59188/5C08A861/t51.2885-15/sh0.08/e35/s640x640/36798423_193353014691121_3785905227794743296_n.jpg" alt="Coffee in Cup">They call classics 'classic' for a reason: because there's nothing wrong with 'em. Batch brewed coffee is always a crowd pleaser and with our selection of locally roasted beans from which to choose, you can bet on guests begging for more.</p>
+          <p style="font-size: 14pt"><img class="img-responsive catering" src="https://ucarecdn.com/d573c1f7-7e71-494d-90a8-81a3e35dafcf/43729884_397495004123378_1734688744095634459_n.jpg" alt="Coffee in Cup">They call classics 'classic' for a reason: because there's nothing wrong with 'em. Batch brewed coffee is always a crowd pleaser and with our selection of locally roasted beans from which to choose, you can bet on guests begging for more.</p>
         </td>
-        <td class="hidden-xs"><img class="img-responsive catering" src="https://scontent-iad3-1.cdninstagram.com/vp/94c15cc6c09ac29881a67bddf4d59188/5C08A861/t51.2885-15/sh0.08/e35/s640x640/36798423_193353014691121_3785905227794743296_n.jpg" alt="Coffee in Cup"></td>
+        <td class="hidden-xs"><img class="img-responsive catering" src="https://ucarecdn.com/d573c1f7-7e71-494d-90a8-81a3e35dafcf/43729884_397495004123378_1734688744095634459_n.jpg" alt="Coffee in Cup"></td>
       </tr>
     </table>
   </div>
