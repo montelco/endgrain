bed80a25fec861f7558b3a0241fdde2c4da2c0c3
Cory Monteleone-Haught <cmonteleoneh@gmail.com>
2019-09-29 22:26:21 -0400

    Update default.blade.php

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		<title>Endgrain Coffee Bar</title>
+		<title>Endgrain Coffee Roasters</title>
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 	    <meta name="mobile-web-app-capable" content="yes">
@@ -19,10 +21,18 @@
 				});
 			});
 		</script>
-	    <meta charset="utf-8">
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    	<meta name="description" content="Located in Southern New Jersey, Endgrain Coffee Bar is the first third wave coffee shop in the area to offer both Nitro on tap and house roasted beans.">
-    	<meta name="keywords" content="Rowan,College,nitro,cold brew,coffee,coffee shop,coffee bar,drinks,cappucino,espresso,latte,Starbucks,Rowan University,Pitman,New Jersey,South Jersey,french press,pourover,Brandywine Coffee Roasters,Royal Mile Coffee Roasters,One Village Coffee Roasters,ReAnimator Coffee Roasters,Philly,Philadelphia,local business">
+	    
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-149020327-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-149020327-1');
+	</script>
+    	<meta name="description" content="Located in Pitman New Jersey, Endgrain Coffee Roasters is the first third wave coffee shop. We serve Nitro Cold Brew on tap and roast our own beans.">
+    	<meta name="keywords" content="Rowan,College,nitro,cold brew,coffee,coffee shop,coffee bar,drinks,cappucino,espresso,latte,Starbucks,Rowan University,Pitman,New Jersey,South Jersey,french press,pourover,Brandywine Coffee Roasters,Royal Mile Coffee Roasters,One Village Coffee Roasters,ReAnimator Coffee Roasters,Philly,Philadelphia,local business,Rowan University,Rowan College of South Jersey,RCSJ,Rowan Engineering,Coffee Bar,Good Coffee">
 	</head>
 	<body>
 		@include('templates.snippets.alerts')
