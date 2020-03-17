ed953c7309bad763f09edfc2f2babc2247d6a080
Cory Monteleone-Haught <cmonteleoneh@gmail.com>
2020-03-17 08:33:48 -0400

    Update alerts.blade.php

--- resources/views/templates/snippets/alerts.blade.php
+++ resources/views/templates/snippets/alerts.blade.php
@@ -1,5 +1,5 @@
 <div data-example-id="dismissible-alert-js">
-	<div class="alert alert-success alert-dismissible fade in" role="alert" style="padding: 0px 15px; font-size: 16px !important;">
+	<div class="alert alert-warning alert-dismissible fade in" role="alert" style="padding: 0px 15px; font-size: 16px !important;">
 		<button type="button" class="close" data-dismiss="alert" aria-label="Close" style="right: 0px; top: 0px;">
 			<span aria-hidden="true">×</span>
 		</button>
